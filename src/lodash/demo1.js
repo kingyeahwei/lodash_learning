88545f3ce2e944a3a9e2447348cb2e4fada80e92
jinyanwei <kingyeahwei@163.com>
2020-09-06 23:57:43 +0800

    init

---
+++ src/lodash/demo1.js
@@ -0,0 +1,16 @@
+// console.log(_.assign({ a: 1 }, { b: 2 }, { c: 3 }));
+// let x = _.map([1, 2, 3], function (item) {
+//   return item * 3;
+// });
+
+// console.log(x);
+
+// console.log(_.chunk(['a', 'b', 'c', 'd'], 2))
+// console.log(_.chunk(['a', 'b', 'c', 'd'], 3))
+
+// console.log(_.compact([0, 1, false, 2, '', 3]))
+// var array = [1];
+// var other = _.concat(array, 2, [3], [[4]]);
+// console.log(other);
+
+console.log(_.difference([3, 2, 1], [4, 2]))
